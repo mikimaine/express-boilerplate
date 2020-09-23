85660d00314d848be4248e6626b0a95095e2f755
mikimaine <mikias.amdu@gmail.com>
2020-09-23 19:13:30 +0300

    inclued express swagger generator

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var jwt = require('express-jwt');
 
 const mongoose = require('./config/mongoose');
-const { jwt_key } = require('./config/vars');
+const { jwt_key, port } = require('./config/vars');
 const { routes } = require('./config/routes');
 
 const { hasPermissions } = require('./middlewares/auth');
@@ -16,6 +16,39 @@ var authRouter = require('./routes/auth');
 
 var app = express();
 
+const expressSwagger = require('express-swagger-generator')(app);
+
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger.json');
+
+let options = {
+    swaggerDefinition: {
+        info: {
+            description: 'This is a simple express boilerplate',
+            title: 'Swagger',
+            version: '1.0.0',
+        },
+        host: `localhost:${port}`,
+        basePath: '/',
+        produces: [
+            "application/json"
+        ],
+        schemes: ['http', 'https'],
+		securityDefinitions: {
+            JWT: {
+                type: 'apiKey',
+                in: 'header',
+                name: 'Authorization',
+                description: "",
+            }
+        }
+    },
+    basedir: __dirname, //app absolute path
+    files: ['./routes/*.js'] //Path to the API handle folder
+};
+
+expressSwagger(options)
+
 // open mongoose connection
 mongoose.connect();
 
@@ -25,10 +58,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// app.use('/api-docs-v2', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(jwt({ secret: jwt_key, algorithms: ['HS256']})
 .unless({path: routes.public})); // Auth
+
 
 // login information state
 app.use('/', indexRouter);
